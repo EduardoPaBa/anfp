b737c6bea3d0cc16f281458712f052d2ca2c31c5
Jemv1 <totto7181@gmail.com>
2020-11-24 22:44:15 -0600

    Grafica

--- resources/views/analisis/ratios.blade.php
+++ resources/views/analisis/ratios.blade.php
@@ -9,12 +9,50 @@
 @endsection
 
 @section('javascript')
-
+	<!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	<script src="{{ asset('js/seleccion.js') }}"></script>
 	<script src="{{ asset('js/ratios.js') }}"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.18/js/bootstrap-select.min.js"></script>
+  	<script type="text/javascript">
+
+      // Load the Visualization API and the corechart package.
+      google.charts.load('current', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        //var uno = $("#inputTotal-razonLiquidez").val();
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+
 @endsection
 
 @section('botones')
@@ -22,7 +60,8 @@
 @endsection
 
 @section('content')
-
+<div id="chart_div"></div>
+<div id="barchart_values" style="width: 900px; height: 300px;"></div>
 <?php
 	$valorCompGrupos="{pasivo}";
 	$valorCompClases="{pasivo corriente}";
